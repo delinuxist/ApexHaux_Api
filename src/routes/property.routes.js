0c651ce06535ae39a1b84f892a1610e447ac01c9
delinuxist <delinuxist.gmail.com>
2022-05-21 08:26:55 +0000

    property route search

--- src/routes/property.routes.js
+++ src/routes/property.routes.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
 const propertyControllers = require("../controllers/property.controller");
 
+router.get("/search", propertyControllers.searchByType);
+
 module.exports = router;
