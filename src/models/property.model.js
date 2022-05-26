496eff95f8c336940a03ad652cd84862fe921b7f
delinuxist <delinuxist@gmail.com>
2022-05-26 00:16:04 +0000

    add create property method

--- src/models/property.model.js
+++ src/models/property.model.js
@@ -61,6 +61,40 @@ class Property {
       cb(result, null);
     });
   }
+
+  static createProperty(newProp, cb) {
+    db.query(
+      `insert into properties values(?,?,?,?,?,?,?,?,?,?)`,
+      [
+        newProp.id,
+        newProp.owner,
+        newProp.status,
+        newProp.price,
+        newProp.state,
+        newProp.city,
+        newProp.address,
+        newProp.type,
+        newProp.image_url,
+        newProp.created_on,
+      ],
+      (err, result) => {
+        if (err) {
+          return cb(err, null);
+        }
+        cb(null, {
+          id: newProp.id,
+          status: newProp.status,
+          price: newProp.price,
+          state: newProp.state,
+          city: newProp.city,
+          address: newProp.address,
+          type: newProp.type,
+          image_url: newProp.image_url,
+          created: newProp.created_on,
+        });
+      }
+    );
+  }
 }
 
 module.exports = Property;
