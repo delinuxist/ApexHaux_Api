af58d874fc94688428313eb209a3ce488573057f
delinuxist <delinuxist.gmail.com>
2022-05-28 17:55:11 +0000

    error handler updated

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -1,4 +1,12 @@
+const CustomAPIError = require("../errors/custom-error");
+
 const errorHandler = (err, req, res, next) => {
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
+  }
   res.status(500).json({
     error: "error",
     message: err.message,
