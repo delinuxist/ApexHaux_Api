c6e6dad6f4161292fe5bc1b74af5df150a20c0c0
delinuxist <delinuxist.gmail.com>
2022-06-01 00:56:44 +0000

    updated delete method

--- src/models/property.model.js
+++ src/models/property.model.js
@@ -35,24 +35,75 @@ class Property {
     });
   }
 
-  static deletePropertyById(propId, cb) {
-    db.query(`delete from properties where id=?`, [propId], (err, result) => {
-      if (err) {
-        return cb(null, err);
+  static deletePropertyById(input, cb) {
+    db.query(
+      `delete from properties where id=? and owner=?`,
+      [input.propId, input.ownerId],
+      (err, result) => {
+        if (err) {
+          return cb(null, err);
+        }
+        cb(result, null);
       }
-      cb(result, null);
-    });
+    );
   }
 
-  static propertyById(prodId, ownerId, cb) {
+  static propertyById(id, cb) {
     db.query(
-      `select * from properties where id=? and owner=?`,
+      `select * from properties where id=?`,
       [prodId, ownerId],
       (err, result) => {
         if (err) {
           return cb(null, err);
         }
         cb(result[0], null);
+      }
+    );
+  }
+
+  static allProperties(ownerId, cb) {
+    db.query(
+      `select * from properties where owner=?`,
+      [ownerId],
+      (err, result) => {
+        if (err) {
+          return cb(null, err);
+        }
+        cb(result, null);
+      }
+    );
+  }
+
+  static createProperty(newProp, cb) {
+    db.query(
+      `insert into properties values(?,?,?,?,?,?,?,?,?,?)`,
+      [
+        newProp.id,
+        newProp.owner,
+        newProp.status,
+        newProp.price,
+        newProp.state,
+        newProp.city,
+        newProp.address,
+        newProp.type,
+        newProp.image_url,
+        newProp.created_on,
+      ],
+      (err, result) => {
+        if (err) {
+          return cb(err, null);
+        }
+        cb(null, {
+          id: newProp.id,
+          status: newProp.status,
+          price: newProp.price,
+          state: newProp.state,
+          city: newProp.city,
+          address: newProp.address,
+          type: newProp.type,
+          image_url: newProp.image_url,
+          created: newProp.created_on,
+        });
       }
     );
   }
